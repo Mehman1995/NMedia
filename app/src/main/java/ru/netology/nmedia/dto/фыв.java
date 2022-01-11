88b2bff5f1142f366c8b248b25d2bafe31f64fb6
Mehman1995 <87534806+Mehman1995@users.noreply.github.com>
2022-01-11 21:18:28 +0300

    Click Like, repost

---
+++ app/src/main/java/ru/netology/nmedia/dto/фыв.java
@@ -0,0 +1,11 @@
+package ru.netology.nmedia.dto;
+
+public class фыв {\фы
+    фывфыв
+        афыаыа
+    фыафыафыа
+
+    public void setФыафыафыа(афыаыа фыафыафыа) {
+        this.фыафыафыа = фыафыафыа;
+    }
+}
